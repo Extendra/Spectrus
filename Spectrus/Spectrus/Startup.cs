9e32ef3add202837c5434bd8c1b3e8b6e1ec7029
Braden Holbrook <braden@extendra.com>
2018-02-20 18:01:03 -0700

    Check in new base specturs web project

---
+++ Spectrus/Spectrus/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace Spectrus
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseExceptionHandler("/error.html");
+
+            var configuration = new ConfigurationBuilder()
+                                    .AddEnvironmentVariables()
+                                    .AddJsonFile(env.ContentRootPath + "/config.json")
+                                    .Build();
+
+            if (configuration.GetValue<bool>("FeatureToggles:EnableDevelperExceptions"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new Exception("ERROR!");
+
+                await next();
+            });
+
+            app.UseMvc();
+
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+            //});
+
+            app.UseFileServer();
+        }
+    }
+}
